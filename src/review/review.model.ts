e65c2ca4cacdf387ca1f52c424b8771d2efb838d
kirillverenih@ya.ru <Kirill Verenih>
2022-10-17 22:16:03 +0400

    add review service

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,5 +1,6 @@
 import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
 import { prop } from '@typegoose/typegoose';
+import { Types } from 'mongoose';
 
 export interface ReviewModel extends Base {
 
@@ -17,4 +18,7 @@ export class ReviewModel extends TimeStamps {
 
   @prop()
   rating: number;
+
+  @prop()
+  productId: Types.ObjectId;
 }
